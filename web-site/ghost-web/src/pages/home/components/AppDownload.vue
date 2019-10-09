643db8606dcee00caf41786f95ab1e410482b84d
yesong <yesong@9air.com>
2019-10-09 10:54:05 +0800

    homepage complete start check api

---
+++ web-site/ghost-web/src/pages/home/components/AppDownload.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="bg-overlay pt-70 pb-70" id="project">
+    <div class="container">
+      <div class="content-details" style="margin-bottom:30px">
+        <h2 class="content-title mx-auto text-uppercase" style="color: #fff">app下载</h2>
+      </div>
+      <div class="row">
+        <div class="col-12 col-md-6">
+          <figure class="projects-picture">
+            <img src="/static/img/youzhang.png" alt="Youzhang">
+          </figure>
+        </div>
+        <div class="col-12 col-md-6">
+          <div class="projects-details">
+            <div class="projects-badge">
+              Featured
+            </div>
+            <h2 class="projects-title">夜半故事app，欢迎下载</h2>
+            <p class="projects-description">
+              <p>夜半故事是一款专门提供鬼故事的应用</p>
+
+              <p>【功能】</p>
+
+              <ol>
+                <li>对鬼故事提供分类：短篇、长篇、校园、医院、家里、民间、灵异、原创、内涵；</li>
+
+                <li>收藏功能</li>
+
+                <li>应用会自动离线缓存你最近看过的十条内容</li>
+
+                <li>清除缓存</li>
+
+                <li>切换主题</li>
+              </ol>
+
+              <p>【意见反馈】</p>
+
+              <p>如果对这款应用有想法和建议，请到关于-反馈告诉我，或直接发邮箱1137660217@qq.com
+
+              本应用源码地址：https://github.com/EL113/GhostStory
+
+              由于技术原因，有些想实现的功能还未上线，将在下一版本推出，敬请期待
+
+              本应用仅用于交流学习，并无商业用途，如果有侵权，请告知修改</p>
+            <div class="projects-cta">
+              <a href="#" class="btn btn-primary" target="_blank">
+                前往下载
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AppDownload'
+}
+</script>
+
+<style scoped>
+
+</style>
--- web-site/ghost-web/src/pages/home/components/Recommend.vue
+++ web-site/ghost-web/src/pages/home/components/Recommend.vue
@@ -23,7 +23,7 @@
                             <li class="page-item page-link">
                                 <font-awesome-icon icon="chevron-left" @click="prePage"/>
                             </li>
-                            <li v-for="pageItem in result.pageList" :key="pageItem" class="page-item page-link" @click="page">
+                            <li v-for="pageItem in result.pageList" :key="pageItem" class="page-item page-link" @click="toPage">
                                 {{pageItem}}
                             </li>
                             <li class="page-item page-link">
@@ -57,6 +57,20 @@ export default {
           1, 2, 3, '...', 15, 16, 17
         ]
       }
+    }
+  },
+  methods: {
+    firstPage () {
+      return 0
+    },
+    prePage () {
+      return 0
+    },
+    nextPage () {
+      return 0
+    },
+    toPage () {
+      return 0
     }
   }
 }
